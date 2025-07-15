7032b96c4b4dd58de52c1c72b9e887845e9254f8
Christopher Penkin <chris@penkin.me>
2025-07-15 15:16:19 +0200

    Section 1: An elixir warmup

---
+++ lib/cards.ex
@@ -0,0 +1,24 @@
+defmodule Cards do
+
+  def create_deck do
+    values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    suits = ["S", "H", "D", "C"]
+
+    for suit <- suits, value <- values do
+      "#{value}-#{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+end
